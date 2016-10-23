70b6e0966b958e60cb43aa15e648c19e18305390
Eric Huynh <ehuynh.school@gmail.com>
2016-10-23 15:06:15 -0500

    Created Course Constants and Updated References to it. updated Gradle Build File to newest version

---
+++ app/src/main/java/JSON/Courses/CourseConstants.java
@@ -0,0 +1,42 @@
+package JSON.Courses;
+
+/**
+ * Created by E-R-C on 10/23/2016.
+ */
+
+public class CourseConstants {
+    /* Variables str Stored: BeginDateTime, Building, CatalogDescription, CourseCode, CourseNumber,
+            CrossListed, CrossListedParent, Days, Division, EndDateTime, EndTime, FastSearch,
+            InstructorFirstName, InstructorLastName, Instructors, LearningDomain, Period, Room,
+            SectionNumber, StartTime, SubjectCode, Term, Title, Year */
+
+    // Integers Stored MaxEnrollment, CurrentEnrollment, capacity;
+    public final static String BGEINDATETIME = "BeginDateTime",
+            BUILDING = "Building",
+            CATALOGDESCRIPTION = "CatalogDescription",
+            COURSECODE = "CourseCode",
+            COURSENUMBER = "CourseNumber",
+            CROSSLISTED = "CrossListed",
+            CROSSLISTEDPARENT = "CrossListedParent",
+            DAYS = "Days",
+            DIVISION = "Division",
+            ENDDATETIME = "EndDateTime",
+            ENDTIME = "EndTime",
+            FASTSEARCH = "FastSearch",
+            INSTRUCTORFIRSTNAME = "InstructorFirstName",
+            INSTRUCTORLASTNAME = "InstructorLastName",
+            INSTRUCTORS = "Instructors",
+            LEARNINGDOMAIN = "LearningDomain",
+            PERIOD = "Period",
+            ROOM = "Room",
+            SECTIONNUMBER = "SectionNumber",
+            STARTTIME = "StartTime",
+            SUBJECTCODE = "SubjectCode",
+            TERM = "Term",
+            TITLE = "Title",
+            YEAR = "Year",
+            MAXENROLLMENT = "MaxEnrollment",
+            CURRENTENROLLMENT = "CurrentEnrollment",
+            CAPACITY = "capacity";
+
+}
